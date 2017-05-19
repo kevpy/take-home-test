b13f263d5cfd57d4d07f81c2e21c94b6fc2bf57f
kevpy <kibetrosh@gmail.com>
2017-05-19 19:19:39 +0300

    Added tests for problem_3.py

--- test_problems.py
+++ test_problems.py
@@ -1,5 +1,6 @@
 from problem_1 import str_transform
 from problem_2 import chunk_string
+from problem_3 import temp_example
 
 
 # test function for problem 1
@@ -11,3 +12,11 @@ def test_problem_2():
     test_text = 'Simple text'
     length = 4
     assert chunk_string(test_text, length) == ['Simp', 'le t', 'ext']
+
+
+def test_problem_3():
+    timestamp = [1, 2, 3, 4, 5, 6, 7]
+    temperature_reading = [9, 2, 3, 5, 8, 1, 10]
+    threshold = 4
+    assert list(
+        temp_example(timestamp, temperature_reading, threshold)) == [4, 7]
